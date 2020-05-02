30dae60a349a7ff25188513d429198c05453bca0
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-05-02 21:01:35 +0900

    save

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $(function(){
-
   $('a[href^="#"]').click(function(){
     var speed = 500;
     var href= $(this).attr("href");
@@ -8,5 +7,74 @@ $(function(){
     $("html, body").animate({scrollTop:position}, speed, "swing");
     return false;
   });
-
 });
+
+
+
+
+function choiceItem() {
+  var popup = document.getElementById('choiceItem-popup');
+  if(!popup) return;
+
+  var blackBg = document.getElementById('choiceItem-black-bg');
+
+  var blackBg = document.getElementById('choiceItem-black-bg');
+  var closeBtn = document.getElementById('choiceItem-close-btn');
+  var showBtn = document.getElementById('choiceItem-show-popup');
+
+  closePopUp(blackBg);
+  closePopUp(closeBtn);
+  closePopUp(showBtn);
+  function closePopUp(elem) {
+    if(!elem) return;
+    elem.addEventListener('click', function() {
+      popup.classList.toggle('is-show');
+    });
+  }
+}
+choiceItem();
+
+function furima() {
+  var popup = document.getElementById('furima-popup');
+  if(!popup) return;
+
+  var blackBg = document.getElementById('furima-black-bg');
+
+  var blackBg = document.getElementById('furima-black-bg');
+  var closeBtn = document.getElementById('furima-close-btn');
+  var showBtn = document.getElementById('furima-show-popup');
+
+  closePopUp(blackBg);
+  closePopUp(closeBtn);
+  closePopUp(showBtn);
+  function closePopUp(elem) {
+    if(!elem) return;
+    elem.addEventListener('click', function() {
+      popup.classList.toggle('is-show');
+    });
+  }
+}
+furima();
+
+function eachfashion() {
+  var popup = document.getElementById('eachfashion-popup');
+  if(!popup) return;
+
+  var blackBg = document.getElementById('eachfashion-black-bg');
+
+  var blackBg = document.getElementById('eachfashion-black-bg');
+  var closeBtn = document.getElementById('eachfashion-close-btn');
+  var showBtn = document.getElementById('eachfashion-show-popup');
+
+  closePopUp(blackBg);
+  closePopUp(closeBtn);
+  closePopUp(showBtn);
+  function closePopUp(elem) {
+    if(!elem) return;
+    elem.addEventListener('click', function() {
+      popup.classList.toggle('is-show');
+    });
+  }
+}
+eachfashion();
+
